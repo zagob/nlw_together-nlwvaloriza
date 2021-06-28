d074f24b23ef759c1710e0d4d1e2a4a38ccc6a2d
zagob <matheusus1_23@hotmail.com>
2021-06-28 14:25:17 -0300

    Aula 03

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
+
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserControler = new CreateUserController();
+const createTagController = new CreateTagController();
 
+router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/users", createUserControler.handle);
 
 export { router }
